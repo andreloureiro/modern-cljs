46f9ce8462f1c0be3de80a20a0be1aa9bc1f4ece
Andre Loureiro <andreloureiroo@gmail.com>
2015-06-28 18:29:28 -0300

    add ring and compojure

---
+++ src/clj/modern_cljs/core.clj
@@ -0,0 +1,12 @@
+(ns modern-cljs.core
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "<p>Hello from compojure</p>")
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(def handler
+  (handler/site app-routes))
